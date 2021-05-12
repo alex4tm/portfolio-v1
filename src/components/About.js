6ee9a5d7b538470a1498b523d7c0ddd5bfe7b658
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-05-12 22:25:57 +0400

    added content and skills and tools placeholder to about section, also css improved a bit

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,21 @@ function About() {
       <h1>A bit about me</h1>
       <div className="about-content">
         <div className="photo"></div>
-        <p>AboutLorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis alias aperiam nam aliquid voluptas, fuga odio magnam dolorum nostrum quo minus similique nemo iure! Aperiam ex velit, dolorum quae non.</p>
+        <div className="about-info">
+          <p></p>
+          <ul> Some random facts about me:
+            <li>Lived and worked in the UAE for the last 10 years. Was lucky enough to get payed and visit the world (more than 70 countries)</li>
+            <li>When i was 12 years old i built a command prompt exec (that was copying in the background some windows folders until the HDD was full) and shared with with 6 of my neighbours on our building network as a prank. I helped them all fix it and had a good laught about it :)</li>
+            <li>I love playing chess, 3 minute and 10 minute time controls-- alex4tm on chess.com :)</li>
+          </ul>
+        </div>
+      </div>
+      <div className="about-skills">
+        <h2>
+          Skills and tools i used in my projects
+        </h2>
+        <div className="skills-photo" href="../images/skills.jpg">
+        </div>
       </div>
     </div>
   );
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -20,7 +20,7 @@ export default function ContactUs() {
         <div>
             <div className="contact-container" id="contact">
             <form onSubmit={sendEmail}>
-                    <h2>Get in touch</h2>
+                    <h1>Get in touch</h1>
                     <div className="contact-wrapper">
                         <div className="contact-field">
                             <input type="text" className="contact-input" placeholder="Name" name="name" required />
