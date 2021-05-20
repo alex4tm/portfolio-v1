84e8384a42b720ed40789f39dc8316cad3ed6963
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-05-20 14:59:19 +0400

    added modal and carousel for project1 - not styled

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Badge from 'react-bootstrap/Badge';
+import Modal from 'react-bootstrap/Modal';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../images/alex.jpg';
 
 function Project1(props) {
+
+  const [lgShow, setLgShow] = useState(false);
   return (
     <>
+<Modal
+        size="lg"
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Large Modal
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+         <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="First slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="Second slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="Third slide"
+    />
+  </Carousel.Item>
+</Carousel>
+        </Modal.Body>
+        <Modal.Footer>
+          <a href="/">View Live</a>
+          <a href="/">View Code</a>
+        </Modal.Footer>
+      </Modal>
+
+
       <li className='projects__item'>
-        <Link className='projects__item__link' to={props.path}>
+        <Link className='projects__item__link' to={props.path} onClick={() => setLgShow(true)}>
           <figure className='projects__item__pic-wrap' data-category={props.label}>
             <img
               className='projects__item__img'
               alt=''
               src={props.src}
+
             />
           </figure>
           <div className='projects__item__info'>
