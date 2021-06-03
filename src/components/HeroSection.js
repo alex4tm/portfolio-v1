cba51365506344c48d99cd098e39f503775a67eb
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-06-03 18:42:27 +0400

    moved hero content to top right side of screen for higher resolution devices

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,23 +9,25 @@ import myPdf from '../images/CV-ILIESI_ALEXANDRU.pdf';
 function HeroSection() {
   return (
     <div className='hero-container' id='home'>
-        <h1>Hello, I'm Alex</h1>
-        <p>Software developer</p>
-        <div className='hero-btns'>
-           <Row>
-              <Col md={{ size: 8, offset: 2 }}>
-                <div >
-                  <a
-                    download={myPdf}
-                    href={myPdf}
-                    className="btn btn--outline btn--medium"
-                  >
-                    Download Resume
-                  <i className="far fa-arrow-alt-circle-down"></i>
-                  </a>
-                </div>
-              </Col>
-            </Row>
+        <div className="hero-content">
+          <h1>Hello, I'm Alex</h1>
+          <p>Software developer</p>
+          <div className='hero-btns'>
+             <Row>
+                <Col md={{ size: 8, offset: 2 }}>
+                  <div >
+                    <a
+                      download={myPdf}
+                      href={myPdf}
+                      className="btn btn--outline btn--medium"
+                    >
+                      Download Resume
+                    <i className="far fa-arrow-alt-circle-down"></i>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+          </div>
         </div>
     </div>
   );
