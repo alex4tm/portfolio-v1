65bb15ec87503d3c3759b8e8b6bdba453995d690
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-06-04 19:40:00 +0400

    added svg path for text antimation arround the theme change feature

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,6 +11,19 @@ function HeroSection() {
     <div className='hero-container' id='home'>
         <div className="hero-content">
           <div className="theme-wrapper">
+
+          <svg width="250" height="280">
+                <animateTransform attributeName="transform" type="translate" from="360 0 0" to="0 0 0" dur="1s" repeatCount="indefinite" />
+            <path id="square_path" d="M50,50 h150 a20,20 0 0 1 20,20 v150 a20,20 0 0 1 -20,20 h-150 a20,20 0 0 1 -20,-20 v-150 a20,20 0 0 1 20,-20 z" fill="none" stroke="white" strokeWidth="1" />
+              <text y="40" font-size="30" font-family="PT Sans, sans-serif">
+                <textPath href="#square_path" id="text_path" class="square_text" startOffset="0">
+                  choose your theme
+                </textPath>
+              </text>
+          </svg>
+          <div id="test">
+            <h3>choose a theme</h3>
+          </div>
             <h3>dark/light</h3>
             <label className="switch">
               <input type="checkbox" />
