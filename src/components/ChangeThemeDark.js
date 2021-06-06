da61ef394d573c43e60c589d72406ae20beb5e30
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-06-06 17:52:21 +0400

    optimised hero section and buttons for theme changing

--- src/components/ChangeThemeDark.js
+++ src/components/ChangeThemeDark.js
@@ -4,7 +4,7 @@ const changeThemeDark = () => {
 
     document.documentElement.style.setProperty('--background', '#121212')
     document.documentElement.style.setProperty('--primary', '#353535')
-    document.documentElement.style.setProperty('--secondary', '#d6dc76de')
+    document.documentElement.style.setProperty('--secondary', '#e8b263')
     document.documentElement.style.setProperty('--hover', '#C2F786ab')
   //   let darkThemeEnabled = document.documentElement.style.setProperty('--primary', 'black');
   //   localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
