c2430d533746cdeb16a1cde33b12de0b52bce135
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-06-06 22:15:42 +0400

    changed dark theme secondary and hover colors

--- src/components/ChangeThemeDark.js
+++ src/components/ChangeThemeDark.js
@@ -4,8 +4,8 @@ const changeThemeDark = () => {
 
     document.documentElement.style.setProperty('--background', '#121212')
     document.documentElement.style.setProperty('--primary', '#353535')
-    document.documentElement.style.setProperty('--secondary', '#e8b263')
-    document.documentElement.style.setProperty('--hover', '#C2F786ab')
+    document.documentElement.style.setProperty('--secondary', '#d1acde')
+    document.documentElement.style.setProperty('--hover', '#a27cafe8')
   //   let darkThemeEnabled = document.documentElement.style.setProperty('--primary', 'black');
   //   localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
 
