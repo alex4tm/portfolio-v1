365c0ebcc169af70e7e0b271c14faf149b68bc85
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-06-07 23:16:26 +0400

    changed modal and carousel color scheme to match theme swapping

--- src/components/ChangeThemeDark.js
+++ src/components/ChangeThemeDark.js
@@ -2,10 +2,10 @@ import './ChangeTheme.css';
 
 const changeThemeDark = () => {
 
-    document.documentElement.style.setProperty('--background', '18, 18, 18')
-    document.documentElement.style.setProperty('--primary', '53, 53, 53')
-    document.documentElement.style.setProperty('--secondary', '187, 133, 248')
-    document.documentElement.style.setProperty('--hover', '219, 177, 250')
+    document.documentElement.style.setProperty('--background', '18, 18, 18') // #121212
+    document.documentElement.style.setProperty('--primary', '53, 53, 53') // #353535
+    document.documentElement.style.setProperty('--secondary', '187, 133, 248') // #BB85F8
+    document.documentElement.style.setProperty('--hover', '219, 177, 250') // #DBB1FA
   //   let darkThemeEnabled = document.documentElement.style.setProperty('--primary', 'black');
   //   localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
 
