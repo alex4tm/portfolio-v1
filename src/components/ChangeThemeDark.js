2e796bc74a8ec78b3bbe3160cfcb207367687508
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-06-09 22:00:44 +0400

    added all color variables for ligh/dark theme swap functions

--- src/components/ChangeThemeDark.js
+++ src/components/ChangeThemeDark.js
@@ -2,10 +2,14 @@ import './ChangeTheme.css';
 
 const changeThemeDark = () => {
 
-    document.documentElement.style.setProperty('--color-background-main', '18, 18, 18') // #121212
-    document.documentElement.style.setProperty('--color-background-second', '53, 53, 53') // #353535
-    document.documentElement.style.setProperty('--color-main-700', '219, 177, 250') // #DBB1FA
-    document.documentElement.style.setProperty('--color-primary', '175,217,219') // #AFD9DB
+    document.documentElement.style.setProperty('--color-background-main', '18, 18, 18'); // #121212
+    document.documentElement.style.setProperty('--color-background-second', '40, 40, 40'); // #282828
+    document.documentElement.style.setProperty('--color-background-third', '60, 60, 60'); // #3C3C3C
+    document.documentElement.style.setProperty('--color-main-700', '219, 177, 250'); // #DBB1FA
+    document.documentElement.style.setProperty('--color-main-500', '199, 155, 228'); // #C79BE4
+    document.documentElement.style.setProperty('--color-main-300', '179, 135, 208'); // #B387D0
+    document.documentElement.style.setProperty('--color-primary', '175,217,219'); // #AFD9DB
+
   //   let darkThemeEnabled = document.documentElement.style.setProperty('--color-background-second', 'black');
   //   localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
 
--- src/components/ChangeThemeLight.js
+++ src/components/ChangeThemeLight.js
@@ -3,10 +3,13 @@ import './ChangeTheme.css';
 
 const changeThemeLight = () => {
 
-    document.documentElement.style.setProperty('--color-background-main', '243, 245, 212')
-    document.documentElement.style.setProperty('--color-background-second', '243, 244, 213')
-    document.documentElement.style.setProperty('--color-main-700', '18, 18, 18')
-    document.documentElement.style.setProperty('--color-primary', '85, 85, 85')
+    document.documentElement.style.setProperty('--color-background-main', '248, 249, 250'); // #F8F9FA
+    document.documentElement.style.setProperty('--color-background-second', '226, 227, 228'); // #E2E3E4
+    document.documentElement.style.setProperty('--color-background-third', '206, 207, 208'); // #CECFD0
+    document.documentElement.style.setProperty('--color-main-700', '18, 18, 18'); // #121212
+    document.documentElement.style.setProperty('--color-main-500', '40, 40, 40'); // #282828
+    document.documentElement.style.setProperty('--color-main-300', '60, 60, 60'); // #3C3C3C
+    document.documentElement.style.setProperty('--color-primary', '166, 10, 13'); // #A00A0D
 
   //   let darkThemeEnabled = document.documentElement.style.setProperty('--color-background-second', 'yellow');
   //   localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
